3bf48cc3d846f3d9b6b01267bc6082d87d04ad35
Meiteusz <contato.matheuswt@gmail.com>
2022-07-15 15:07:53 -0300

    Add project files.

---
+++ src/EmailSender.API/Controllers/EmailSenderController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmailSender.API.Controllers
+{
+    [ApiController]
+    public class EmailSenderController : Controller
+    {
+        [HttpGet]
+        [Route("/emailSender/api/v1/sendEmail")]
+        public async Task<IActionResult> SendEmail()
+        {
+            return Ok(new 
+            {
+                Email = "EmailTeste@gmail.com.br",
+                Teste = "TesteAqui"
+            }); ;
+        }
+    }
+}
